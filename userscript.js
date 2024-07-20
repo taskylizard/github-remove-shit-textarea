a92ccf7074d7976ba0a347fa457268d5edcff376
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-07-20 22:21:57 +0530

    chore: init

---
+++ userscript.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Disable GitHub textarea codeframe
+// @version      0.1.0
+// @license      MIT
+// @homepageURL  https://github.com/taskylizard/github-remove-shit-textarea
+// @description  Disables the garbage textarea.
+// @author       taskylizard (https://github.com/taskylizard)
+// @match        https://*.github.com/*
+// @run-at       document-end
+// ==/UserScript==
+
+const selector = "#read-only-cursor-text-area";
+
+function callback(mutationList, _observer) {
+  mutationList.forEach((mutation) => {
+    mutation.addedNodes.forEach((node) => {
+      if (node.querySelector && node.querySelector(selector)) {
+        node.querySelector(selector).disabled = true;
+      }
+    });
+  });
+}
+
+const observer = new MutationObserver(callback);
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
